50fa8998c937878b928755f80714c8796ed50b57
nzuniga89-jolu <n.zuniga89@gmail.com>
2026-08-27 00:45:57 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,29 @@
-const CACHE = 'natalia-v3';
+const CACHE = 'natalia-v4';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ])
-    )
-  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
+        keys.map(key => caches.delete(key))
       )
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
+  // Las páginas HTML siempre se cargan desde Internet
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request, { cache: 'no-store' })
+    );
+    return;
+  }
+
+  // Para el resto de archivos: Internet primero
   event.respondWith(
     fetch(event.request)
       .then(response => {
